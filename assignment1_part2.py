84e241c0e801653a838a10f078facb2d04f426fb
Synch1216 <Synch1216@gmail.com>
2018-09-03 22:50:04 -0400

    File

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,33 @@
+# class Book:
+#     def __init__(self):
+#         title= raw_input("What is the title of the book?:").strip()
+#         author = raw_input("Who is the author of the book?:").strip()
+#         print ("The Book Of The Year Is: {}, Written By: {}").format(title, author)
+#
+#
+# Book1= Book()
+#
+# Book1
+#
+
 class Book:
-    def __init__(self):
-        title= raw_input("What is the title of the book?:").strip()
-        author = raw_input("Who is the author of the book?:").strip()
-        print ("The Book Of The Year Is: {}, Written By: {}").format(title, author)
+    title =  ""
+    author = ""
+    def display (self):
+        desc = " %s is the title of the book written by %s ." % (self.title, self.author)
+        print desc
+
+book1= Book()
+book1.title = "Of Mice & Men"
+book1.author = "John Steinbeck"
+book1.display()
+
+book2= Book()
+book2.title = "To Kill A Mockingbird"
+book2.author = "Harper Lee"
+book2.display()
 
 
-Book1= Book()
 
-Book1
+
 
